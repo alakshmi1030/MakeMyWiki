7da34cbe3f51ce25cab11a47566c42ac4f551b28
Anand Lakshminarayan <anand.lakshminarayan@gmail.com>
2015-02-02 11:08:21 -0800

    removed flask-social dependencies

--- start.py
+++ start.py
@@ -2,14 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from backend import friendSearch
-from flask.ext.social import Social
-from flask.ext.social.datastore import SQLAlchemyConnectionDatastore
 
 app = Flask("start")
-app.config['SOCIAL_FACEBOOK'] = {
-    'consumer_key': '764244473658268',
-    'consumer_secret': 'c2d3bf09dcc94bc043fa27c5fe9a9e95'
-}
 
 @app.route("/")
 def index():
